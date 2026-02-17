73ee390d02b35ac0b531e3ebf7a576fb4c779f4c
radhikamadhavi369 <radhikamadhavi367@gmail.com>
2026-02-17 14:42:49 +0530

    Update StepperMotor.py

--- StepperMotor.py
+++ StepperMotor.py
@@ -30,3 +30,36 @@ while True:
     in3.value(0)
     in4.value(1)
     time.sleep(0.005)
+
+
+Steppermotor rotating clock wise and aniticlockwise ;
+
+from machine import Pin
+import time
+
+in1 = Pin(13,Pin.OUT)
+in2 = Pin(14,Pin.OUT)
+in3 = Pin(15,Pin.OUT)
+in4 = Pin(19,Pin.OUT)
+
+numbers = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+nb = [[0,0,0,1],[0,0,1,0],[0,1,0,0],[0,0,0,0]]
+
+while True:
+    for k in range(500):
+        for i in numbers:
+            in.value(i[0])
+            in.value(i[1])
+            in.value(i[2])
+            in.value(i[3])
+            time.sleep_ms(5)
+        
+    for x in range(500):
+        for s in r_di:
+            in.value(s[0])
+            in.value(s[1])
+            in.value(s[2])
+            in.value(s[3])
+            time.sleep_ms(5)
+            
+    
